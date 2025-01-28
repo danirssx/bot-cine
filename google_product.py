b0f153fd1fd16bc015ddac3ac8ec2073017c6111
danirssx <rossdaniel5018@gmail.com>
2025-01-28 00:59:29 -0400

    images added

--- google_product.py
+++ google_product.py
@@ -4,8 +4,11 @@ from PIL import Image
 from io import BytesIO
 from keys import SEARCH_ENGINE_ID, GOOGLE_API_KEY
 import requests
+import re
+import os
 
-query = "REEFLO PREMIUM PUMPS DART-SNAPPER HYBRID 4200-2600 GPH EXTERNAL PUMP"
+query = "TETRA® POND DEBRIS HANDLING PUMP – 3517 GPH – FOR PONDS UP TO 4000 GALLONS"
+save_folder = "images"
 
 # Google Custom Search API endpoint
 url = f"https://www.googleapis.com/customsearch/v1"
@@ -24,26 +27,36 @@ if response.status_code == 200:
     data = response.json()
 
     if "items" in data:
-        image_url = data["items"][0]["link"]
-        print(f"Image URL: {image_url}")
+        images = []
+        for index, item in enumerate(data["items"]):
+            image_url = item["link"]
 
-        image_response = requests.get(image_url)
+            image_response = requests.get(image_url)
 
-        if image_response.status_code == 200:
+            if image_response.status_code == 200:
 
-            image = Image.open(BytesIO(image_response.content))
+                image = Image.open(BytesIO(image_response.content))
 
-            if image.mode == "RGBA":
-                background = Image.new("RGB", image.size, (255, 255, 255))  # White background
-                background.paste(image, mask=image.split()[3])  # Use alpha channel as mask
-                image = background
+                if image.mode == "RGBA":
+                    background = Image.new("RGB", image.size, (255, 255, 255))  # White background
+                    background.paste(image, mask=image.split()[3])  # Use alpha channel as mask
+                    image = background
 
-            image.save(f"{query}.jpg")
+                # Sanitize
+                safe_query = re.sub(r'[^\w\s-]', '', query).replace(" ", "_")
+                file_name = f"{safe_query}_{index + 1}.jpg"
+                file_path = os.path.join(save_folder, file_name)
 
-            image.show()
+                # Save
+                image.save(file_path)
 
-        else:
-            print("Failed to download thge image")
+                images.append(file_name)
+                image.show()
+
+                print(image_url)
+
+            else:
+                print("Failed to download thge image")
 
     else:
         print("No image founded")
